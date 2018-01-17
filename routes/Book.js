4767119f6c5fe143a4e17b09c9dfc3f6075560b4
monukanyal <monu_kanyal@esferasoft.com>
2018-01-17 13:34:15 +0530

    UPDATED 17 JAN

--- routes/Book.js
+++ routes/Book.js
@@ -2,79 +2,65 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 var Book=require('../Models/Book.js'); //including model
+var VerifyToken = require('../verifytoken'); //api middleware
 
-router.get('/',function(req,res){
-	Book.find({}).exec(function(err,results){
-		if(err)
-		{
-			res.status(400).json({ error: err });
-		}
-		else
-		{
-			res.status(200).json({ data: results });
-		}
+router.get('/',VerifyToken,function(req,res){
+	//req.userId
+	
+	Book.find({}).exec().then((results)=>{
+		res.status(200).json({ data: results });
+	}).catch((err)=>{
+		res.status(400).json({ error: err });
 	});
 });
 
+
 router.post('/',function(req,res){
+
 	var newBook=new Book();
 	newBook.Title=req.body.Title;
 	newBook.Author=req.body.Author;
 	newBook.Category=req.body.Category;
-	newBook.save(function(err,results){
-		if(err)
-		{
-			res.status(400).json({ error: err });
-		}
-		else
-		{
-			res.status(200).json({ data: results });
-		}
+	newBook.save().then((results)=>{
+		res.status(200).json({ data: results });
+	}).catch((err)=>{
+		res.status(400).json({ error: err });
 	});
 });
 
+
 router.get('/:id',function(req,res){
-	Book.find({_id:req.params.id}).exec(function(err,results){
-		if(err)
-		{
+
+	Book.find({_id:req.params.id}).exec().then((results)=>{
+		res.status(200).json({ data: results });
+	}).catch((err)=>{
 			res.status(400).json({ error: err });
-		}
-		else
-		{
-			res.status(200).json({ data: results });
-		}
 	});
 });
 
 
 router.put('/:id',function(req,res){
-	Book.findOneAndUpdate({_id:req.params.id},{$set: {Title:req.body.Title}},{upsert:true}, function(err,results){
-		if(err)
-		{
-			res.status(400).json({ error: err });
-		}
-		else
-		{
-			res.status(200).json({ data: results });
-		}
-	});
+	  var query = Book.findOneAndUpdate({_id:req.params.id},{$set: {Title:req.body.Title}},{upsert:true});
+		 query.exec().then((results)=> {
+	     	res.status(200).json({ status:'success',data: results });
+	    }).catch((err)=>{
+
+	    	res.status(400).json({ status:'error',data: err });
+	    });
+	
 });
 
 
 router.delete('/:id',function(req,res){
-	Book.findOneAndRemove({_id:req.params.id}, function(err,results){
-		if(err)
-		{
-			res.status(400).json({ error: err });
-		}
-		else
-		{
-			res.status(200).json({ data: results });
-		}
+	
+	var query=Book.findOneAndRemove({_id:req.params.id});
+	query.exec().then((results)=>{
+		res.status(200).json({ data: results });
+	}).catch((err)=>{
+		res.status(400).json({ error: err });
 	});
+	
 });
-
-
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const passport = require('passport');
+const md5 = require('md5');
 const router = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/');
 var User=require('../Models/user.js'); //including model
+var jwt = require('jsonwebtoken');
 /* GET home page. */
 
 router.get('/', function (req, res) {
@@ -21,7 +23,7 @@ router.get('/logout', ensureLoggedIn, function(req, res, next) {
 
 
 
-  router.get('/profile', ensureLoggedIn, function(req, res, next) {
+router.get('/profile', ensureLoggedIn, function(req, res, next) {
     User.find({_id:req.session.passport.user._id}).exec(function(err,results){
     if(err){
       console.log(err);
@@ -33,6 +35,33 @@ router.get('/logout', ensureLoggedIn, function(req, res, next) {
     
   });
 }); 
+
+//for api
+router.post('/apilogin',function(req,res){
+     var query=User.findOne({ email:req.body.username,password:md5(req.body.password) });
+     query.exec().then((userdata)=>{
+      if (!userdata) {
+        res.status(201).json({ auth:false,result: userdata });
+      }
+      else
+      {
+         var token = jwt.sign({ id: userdata._id },'supersecret', {
+          expiresIn: 86400 // expires in 24 hours
+        });
+         var obj={};
+         obj._id=userdata._id;
+         obj.Name=userdata.Name;
+         obj.email=userdata.email;
+         res.status(200).json({ auth:true,result:obj ,token:token, token_expire_on:'86400' });
+      }
+
+     }).catch((err)=>{
+
+      res.status(400).json({ error: err });
+
+     });
+    
+});
 
 /*
 router.get('/', function(req, res, next) {
