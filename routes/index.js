f2bc7d9b14a333c3bb6a5f53000dff0415451c60
monukanyal <monu_kanyal@esferasoft.com>
2018-01-30 12:19:13 +0530

    Revert "update 30 jan"
    This reverts commit 871b5ee32fae1b1499ee5baf83bee75729e01f78.

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,13 @@ router.get('/', function (req, res) {
 });
 
 router.post('/login',passport.authenticate('local', { successRedirect: '/dashboard', failureRedirect: '/', failureFlash: 'Please provide correct username and password!!',successFlash: 'Welcome!'  }));
+
 router.get('/logout', ensureLoggedIn, function(req, res, next) {
     req.session.destroy();
     //res.json({ status: 'done' });
     res.redirect('/');
 }); 
 
-
-router.get('/auth/google',passport.authenticate('google', { scope: ['profile','email'] }));
-router.get('/auth/google/callback', 
-  passport.authenticate('google', { successRedirect: '/dashboard', failureRedirect: '/', failureFlash: 'Something is wrong,please try again later',successFlash: 'Welcome!' }),
-  function(req, res) {
-    res.redirect('/dashboard');
-  });
 
 
 router.get('/profile', ensureLoggedIn, function(req, res, next) {
@@ -42,9 +36,8 @@ router.get('/profile', ensureLoggedIn, function(req, res, next) {
   });
 }); 
 
-//api 1- will return token 
+//for api
 router.post('/apilogin',function(req,res){
-  //console.log(req.header);
      var query=User.findOne({ email:req.body.username,password:md5(req.body.password) });
      query.exec().then((userdata)=>{
       if (!userdata) {
@@ -69,8 +62,6 @@ router.post('/apilogin',function(req,res){
      });
     
 });
-
-
 
 /*
 router.get('/', function(req, res, next) {
