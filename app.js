8af5c11e6cb6c14919251024768db17b79d165df
monukanyal <monu_kanyal@esferasoft.com>
2018-02-12 18:43:18 +0530

    khallli

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 var app = express();
 const path = require('path');
 const md5=require('md5');
+var mv = require('mv');  //its for chokidar
 //var morgan = require('morgan'); //http request logger
 const flash = require('connect-flash');
 var mongoose=require('mongoose');
+ var chokidar = require('chokidar');
+var sysPath = require('path');
+const fileUpload = require('express-fileupload');
+var Song=require('./Models/Songs.js'); //including model
 
 /*---------------------------
 
@@ -14,7 +19,7 @@ var url ="mongodb://Esfera:esfera456@ds133547.mlab.com:33547/esferasoft";
 
 ------------------------------*/
 
-var port =process.env.PORT || 8080;;
+var port =process.env.PORT || 8080;
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
        http.listen(port);
@@ -187,6 +192,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'fav.png')));
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -216,6 +222,74 @@ app.use('/dashboard', Users);
 app.use('/books', Book);
 
 /*---------routes end---------------------*/
+
+/*--chokiii---*/
+var paths="/Users/macmini/Downloads/alldownloads"
+var j=0;
+chokidar.watch(paths, {usePolling: true,
+  interval: 100,
+  atomic:true }).on('all', function(event, file) {
+    //console.log(event+' :'+file);
+
+   if (path.extname(file) === '.mp3') {    
+      var newfile=file.replace(path.basename(file), "");
+        console.log("Mp3 path: " + newfile);
+        console.log("event: " + event);
+      
+      if(event=="add" || event =="change" || event =="addDir") 
+      {
+         
+            console.log("Mp3 basename: " + path.basename(file)) ;
+             mv(file, "./public/audio/"+path.basename(file), {clobber: false},function(err) {
+                console.log(err);
+                if(err!=null)
+                {
+                      if(err['code']=='EEXIST')
+                      {
+                        console.log('already exist file');
+                      }
+                
+               }
+               else
+                {
+                   //db update 
+                   console.log('db updating');
+                   var newSong=new Song();
+                      newSong.Title=path.basename(file);
+                      newSong.filepath=path.basename(file);
+                      newSong.Category='Normal';
+                      newSong.save().then((results)=>{
+                         console.log('db updated:'+results);
+                      }).catch((err)=>{
+                        console.log('db updated err:'+err);
+                      });
+                }
+                
+              });
+         
+               // fs.readFile(file, 'utf8', function (err, data) {
+               //      if (err) throw err;
+               //      var obj = JSON.parse(data);               
+               //      var Title=obj.title;
+               //      var mpdpath=newfile+'Manifest.mpd';
+               //        j++;
+               //        var index = queue._queue.map(function (e) { return e.title; }).indexOf(Title);
+               //          if (index === -1) 
+               //          {     
+               //              queue.push({ title: Title, path:mpdpath, metajson:obj }, 3500*j);             
+               //          }
+               //      }); 
+              
+        }
+        // else if (event == 'unlink')
+        // {     
+        //    console.log("removing MP3");
+        //      //movieService.removeData(file);
+     
+        // }
+   }
+});
+  /*---chokiii end--*/
 
 /*io.on('connection', function(socket){
 
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,18 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/');
 const router = express.Router();
+var async=require('async');
 var User=require('../Models/user.js'); //including model
+var Song=require('../Models/Songs.js'); //including model
+var search = require('youtube-search');
+
+
+var opts = {
+  maxResults: 8,
+  key: 'AIzaSyDnjiB1yHIkT3BDVTpf80LqC3fMZ8_ygIU'
+};
+
+
 /* var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url ="mongodb://Esfera:esfera456@ds133547.mlab.com:33547/esferasoft"; */
@@ -12,10 +23,51 @@ var url ="mongodb://Esfera:esfera456@ds133547.mlab.com:33547/esferasoft"; */
 /* GET user dashbard. */
 router.get('/', ensureLoggedIn, function(req, res, next) 
 {
+	 //var arr="[{title: 'Happy Forever Alone Day',file: 'Happy Forever Alone Day (Forever Alone Song)',howl: null},{title:'In The End',file:'In The End _Official Video_ - Linkin Park',howl:null},{title:'Swag Se Swagat Song',file:'Swag Se Swagat Song',howl:null}]";
+	Song.find({}).sort([['createdAt', 'descending']]).exec().then((results)=>{
+	   var mainarr=[];
+	   var i=0;
+	 	
+		async.each(results, function(result, callback) {
+			    var myObj = {};
+			       i=i+1;
+					myObj.title = result.filepath;
+					myObj.file = result.filepath;
+					myObj.howl = null;
+					mainarr.push(myObj);
+					if(i==parseInt(results.length))
+					{
+					  callback(mainarr,null);
+					}
+			}, function(result,err) {
+			  
+			    if( err ) {
+			      console.log('failed to process');
+			    } else {
+			      res.render('dashboard',{message:req.flash('success'),Name:req.session.passport.user.Name,playerlist:JSON.stringify(result)});
+			    }
+			});  
+	
+		//
+	}).catch((err)=>{
+		console.log(err);
+		res.render('dashboard'	,{message:req.flash('success'),Name:req.session.passport.user.Name,playerlist:''});
+	});
 	//console.log('session email:'+req.session.passport.user.Name);
-    var arr="[{title: 'Happy Forever Alone Day',file: 'Happy Forever Alone Day (Forever Alone Song)',howl: null},{title:'In The End',file:'In The End _Official Video_ - Linkin Park',howl:null},{title:'Swag Se Swagat Song',file:'Swag Se Swagat Song',howl:null}]";
+   
 
-	res.render('dashboard'	,{message:req.flash('success'),Name:req.session.passport.user.Name,playerlist:arr});
+	
+});
+
+router.post('/search', ensureLoggedIn, function(req, res, next) 
+{
+	var searchterm=req.body.query;
+
+	search(searchterm, opts, function(err, results) {
+	  if(err) return console.log(err);
+	      //console.log(results);
+          res.status(200).json({ term:searchterm,data:results });
+	});
 });
 
 
